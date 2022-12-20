8c1d84f1f5daec65cac60413472a62b69ecb5712
Yohan de Rose <yohanderose@gmail.com>
2022-12-20 07:43:14 +0700

    feat: Adding osx ffmpeg output script

--- api/play_sound.sh
+++ api/play_sound.sh
@@ -1,9 +1,17 @@
 #! /bin/sh
+# ffmpeg play audio_file on specified channel of speaker
 audio_file=$1
 output_channel=$2
 output_device=1
 
 
-# ffmpeg play audio_file on specified channel of speaker
-ffmpeg -i $audio_file -ac 2 -filter_complex "[0:a]pan=stereo|c$output_channel=c0[a]" -map "[a]" -f alsa hw:$output_device,0
+# if linux
+if [ "$(uname)" == "Linux" ]; then
+	ffmpeg -i $audio_file -ac 2 -filter_complex "[0:a]pan=stereo|c$output_channel=c0[a]" -map "[a]" -f alsa hw:$output_device,0
 
+fi
+
+# if mac
+if [ "$(uname -s)" == "Darwin" ]; then
+	ffmpeg -i $audio_file -ac 2 -filter_complex "[0:a]pan=stereo|c$output_channel=c0[a]" -map "[a]" -f audiotoolbox -audio_device_index 1 -
+fi
