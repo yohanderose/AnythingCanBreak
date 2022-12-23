45a76e80f27dc9a1baeed31e5fb6e8e325db2b75
Yohan de Rose <yohanderose@gmail.com>
2022-12-23 16:45:30 +0700

    feat: Fading ffmpeg out before killing process

--- api/play_sound.sh
+++ api/play_sound.sh
@@ -1,14 +1,32 @@
-#! /bin/sh
+#! /usr/bin/bash
 # ffmpeg play audio_file on specified channel of speaker
 audio_file=$1
 output_channel=$2
 output_device=1
 
+# Dims the speaker before killing ffmpeg_pid
+# returns the volume level to the original level
+function fade_out  {
+	# get the current volume
+	current_volume=$(amixer -c $output_device sget Master | grep -o -P '(?<=\[)[0-9]+(?=%\])')
+
+	for ((i=$current_volume; i>=0; i-=10)); do
+		amixer -c $output_device sset Master $i%
+		sleep 0.1
+	done
+
+	# kill the process
+	kill -9 $(ps -aux | grep "ffmpeg.*api/sound/4/speaker1.wav" | grep -v grep | awk '{print $2}')
+	# reset volume
+	amixer -c $output_device sset Master $current_volume%
+}
 
 # if linux
 if [ "$(uname)" == "Linux" ]; then
-	ffmpeg -i $audio_file -ac 2 -filter_complex "[0:a]pan=stereo|c$output_channel=c0[a];[a]dynaudnorm=p=0.9:s=5[a_norm]" -map "[a_norm]" -f alsa hw:$output_device,0
-
+	ffmpeg -i $audio_file -ac 2 -filter_complex "[0:a]pan=stereo|c$output_channel=c0[a];[a]dynaudnorm=p=0.9:s=5[a_norm]" -map "[a_norm]" -f alsa hw:$output_device,0 -loglevel quiet &
+ 
+	sleep 2
+	fade_out
 fi
 
 # if mac
