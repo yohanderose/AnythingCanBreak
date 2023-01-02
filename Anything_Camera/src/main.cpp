ebf57f46d85995e56172c5ac69d69e55086a1462
Yohan de Rose <yohanderose@gmail.com>
2023-01-02 15:07:03 +0700

    feat: ESP32 connecting to wireless network and serving video frames webserver

--- Anything_Camera/src/main.cpp
+++ Anything_Camera/src/main.cpp
@@ -1,5 +1,6 @@
 #include "ESPAsyncWebServer.h"
 #include "WiFi.h"
+#include "camera_index.h"
 #include "esp_camera.h"
 #include <Arduino.h>
 /* #include <stdio.h> */
@@ -56,10 +57,41 @@
 // Enter your WiFi credentials
 // ===========================
 const char *ssid = "yohan_phone";
-const char *password = "testing123";
+const char *pass = "testing123";
 int secondsElapsed = 0;
 
 void startCameraServer();
+
+int connectToWiFi() {
+
+  WiFi.mode(WIFI_STA);
+  // check for the presence of the shield:
+  /* if (WiFi.status() == WL_NO_SHIELD) { */
+  /*   Serial.println("WiFi shield not present"); */
+  /*   // don't continue: */
+  /*   while (true) */
+  /*     ; */
+  /* } */
+
+  Serial.print("Attempting to connect to WPA SSID: ");
+  Serial.print(ssid);
+  Serial.print(" ...");
+  int attempts = 0;
+  // attempt to connect to Wifi network:
+  WiFi.begin(ssid, pass);
+  while (WiFi.status() != WL_CONNECTED) {
+    if (attempts++ > 5) {
+      Serial.println("FAILED");
+      Serial.println("Restarting ESP32...");
+      ESP.restart();
+    }
+    delay(5000);
+    Serial.print(".");
+  }
+  Serial.println("CONNECTED");
+
+  return 0;
+}
 
 void setup() {
   pinMode(LED_GPIO_NUM, OUTPUT);
@@ -68,97 +100,89 @@ void setup() {
   /* Serial.setDebugOutput(true); */
   Serial.println();
 
-  /* camera_config_t config; */
-  /* config.ledc_channel = LEDC_CHANNEL_0; */
-  /* config.ledc_timer = LEDC_TIMER_0; */
-  /* config.pin_d0 = Y2_GPIO_NUM; */
-  /* config.pin_d1 = Y3_GPIO_NUM; */
-  /* config.pin_d2 = Y4_GPIO_NUM; */
-  /* config.pin_d3 = Y5_GPIO_NUM; */
-  /* config.pin_d4 = Y6_GPIO_NUM; */
-  /* config.pin_d5 = Y7_GPIO_NUM; */
-  /* config.pin_d6 = Y8_GPIO_NUM; */
-  /* config.pin_d7 = Y9_GPIO_NUM; */
-  /* config.pin_xclk = XCLK_GPIO_NUM; */
-  /* config.pin_pclk = PCLK_GPIO_NUM; */
-  /* config.pin_vsync = VSYNC_GPIO_NUM; */
-  /* config.pin_href = HREF_GPIO_NUM; */
-  /* config.pin_sscb_sda = SIOD_GPIO_NUM; */
-  /* config.pin_sscb_scl = SIOC_GPIO_NUM; */
-  /* config.pin_pwdn = PWDN_GPIO_NUM; */
-  /* config.pin_reset = RESET_GPIO_NUM; */
-  /* config.xclk_freq_hz = 20000000; */
-  /* config.frame_size = FRAMESIZE_UXGA; */
-  /* config.pixel_format = PIXFORMAT_JPEG; // for streaming *1/ */
-  /* // config.pixel_format = PIXFORMAT_RGB565; // for face detection */
-  /* config.grab_mode = CAMERA_GRAB_WHEN_EMPTY; */
-  /* config.fb_location = CAMERA_FB_IN_PSRAM; */
-  /* config.jpeg_quality = 12; */
-  /* config.fb_count = 1; */
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.frame_size = FRAMESIZE_UXGA;
+  config.pixel_format = PIXFORMAT_JPEG; // for streaming */
+  // config.pixel_format = PIXFORMAT_RGB565; // for face detection
+  config.grab_mode = CAMERA_GRAB_WHEN_EMPTY;
+  config.fb_location = CAMERA_FB_IN_PSRAM;
+  config.jpeg_quality = 12;
+  config.fb_count = 1;
 
-  /* // if PSRAM IC present, init with UXGA resolution and higher JPEG quality
-   */
+  if (config.pixel_format == PIXFORMAT_JPEG) {
+    if (psramFound()) {
+      config.jpeg_quality = 10;
+      config.fb_count = 2;
+      config.grab_mode = CAMERA_GRAB_LATEST;
+    } else {
+      // Limit the frame size when PSRAM is not available
+      config.frame_size = FRAMESIZE_SVGA;
+      config.fb_location = CAMERA_FB_IN_DRAM;
+    }
+  } else {
+    // Best option for face detection/recognition
+    config.frame_size = FRAMESIZE_240X240;
+#if CONFIG_IDF_TARGET_ESP32S3
+    config.fb_count = 2;
+#endif
+  }
 
-  /* //                      for larger pre-allocated frame buffer. *1/ */
-  /* if (config.pixel_format == PIXFORMAT_JPEG) { */
-  /*   if (psramFound()) { */
-  /*     config.jpeg_quality = 10; */
-  /*     config.fb_count = 2; */
-  /*     config.grab_mode = CAMERA_GRAB_LATEST; */
-  /*   } else { */
-  /*     // Limit the frame size when PSRAM is not available */
-  /*     config.frame_size = FRAMESIZE_SVGA; */
-  /*     config.fb_location = CAMERA_FB_IN_DRAM; */
-  /*   } */
-  /* } else { */
-  /*   // Best option for face detection/recognition */
-  /*   config.frame_size = FRAMESIZE_240X240; */
-  /* #if CONFIG_IDF_TARGET_ESP32S3 */
-  /*   config.fb_count = 2; */
-  /* #endif */
-  /* } */
+#if defined(CAMERA_MODEL_ESP_EYE)
+  pinMode(13, INPUT_PULLUP);
+  pinMode(14, INPUT_PULLUP);
+#endif
 
-  /* #if defined(CAMERA_MODEL_ESP_EYE) */
-  /* pinMode(13, INPUT_PULLUP); */
-  /* pinMode(14, INPUT_PULLUP); */
-  /* #endif */
+  // camera init */
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    return;
+  }
+  sensor_t *s = esp_camera_sensor_get();
+  // initial sensors are flipped vertically and colors are a bit saturated
+  if (s->id.PID == OV3660_PID) {
+    s->set_vflip(s, 1);       // flip it back
+    s->set_brightness(s, 1);  // up the brightness just a bit
+    s->set_saturation(s, -2); // lower the saturation
+  }
+  // drop down frame size for higher initial frame rate
+  if (config.pixel_format == PIXFORMAT_JPEG) {
+    s->set_framesize(s, FRAMESIZE_QVGA);
+  }
+#if defined(CAMERA_MODEL_M5STACK_WIDE)
+  defined(CAMERA_MODEL_M5STACK_ESP32CAM) s->set_vflip(s, 1);
+  s->set_hmirror(s, 1);
+#endif
+#if defined(CAMERA_MODEL_ESP32S3_EYE)
+  s->set_vflip(s, 1);
+#endif
 
-  /* // camera init *1/ */
-  /* esp_err_t err = esp_camera_init(&config); */
-  /* if (err != ESP_OK) { */
-  /*   Serial.printf("Camera init failed with error 0x%x", err); */
-  /*   return; */
-  /* } */
-  /* sensor_t *s = esp_camera_sensor_get(); */
-  /* // initial sensors are flipped vertically and colors are a bit saturated */
-  /* if (s->id.PID == OV3660_PID) { */
-  /*   s->set_vflip(s, 1);       // flip it back */
-  /*   s->set_brightness(s, 1);  // up the brightness just a bit */
-  /*   s->set_saturation(s, -2); // lower the saturation */
-  /* } */
-  /* // drop down frame size for higher initial frame rate */
-  /* if (config.pixel_format == PIXFORMAT_JPEG) { */
-  /*   s->set_framesize(s, FRAMESIZE_QVGA); */
-  /* } */
-  /* #if defined(CAMERA_MODEL_M5STACK_WIDE) */
-  /* defined(CAMERA_MODEL_M5STACK_ESP32CAM) s->set_vflip(s, 1); */
-  /* s->set_hmirror(s, 1); */
-  /* #endif */
-  /* #if defined(CAMERA_MODEL_ESP32S3_EYE) */
-  /* s->set_vflip(s, 1); */
-  /* #endif */
-  /* WiFi.begin(ssid, password); */
-  /* WiFi.setSleep(false); */
-  /* while (WiFi.status() != WL_CONNECTED) { */
-  /*   delay(500); */
-  /*   Serial.print("."); */
-  /* } */
-  /* Serial.println(""); */
-  /* Serial.println("WiFi connected"); */
-  /* startCameraServer(); */
-  /* Serial.print("Camera Ready! Use 'http://"); */
-  /* Serial.print(WiFi.localIP()); */
-  /* Serial.println("' to connect"); */
+  // Init WiFi
+  connectToWiFi();
+
+  startCameraServer();
+  Serial.print("Camera Ready! Use 'http://");
+  Serial.print(WiFi.localIP());
+  Serial.println("' to connect");
 }
 
 void loop() {
@@ -170,5 +194,5 @@ void loop() {
   delay(300);
 
   /* printf("seconds elapsed: %d", ++secondsElapsed); */
-  Serial.println("seconds elapsed: " + String(++secondsElapsed));
+  /* Serial.println("seconds elapsed: " + String(++secondsElapsed)); */
 }
