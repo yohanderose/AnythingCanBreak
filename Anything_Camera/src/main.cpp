c1127f7002841a3857dfbb969c2abb25ee5e5366
Yohan de Rose <yohanderose@gmail.com>
2023-01-02 18:23:04 +0700

    feat: ESP32 streaming video feed to local ip

--- Anything_Camera/src/main.cpp
+++ Anything_Camera/src/main.cpp
@@ -1,8 +1,14 @@
-#include "ESPAsyncWebServer.h"
 #include "WiFi.h"
 #include "camera_index.h"
 #include "esp_camera.h"
+#include "esp_err.h"
+#include "esp_http_server.h"
+#include "fb_gfx.h"
+#include "sensor.h"
+#include "soc/rtc_cntl_reg.h" //disable brownout problems
+#include "soc/soc.h"          //disable brownout problems
 #include <Arduino.h>
+#include <AsyncTCP.h>
 /* #include <stdio.h> */
 
 //
@@ -53,14 +59,106 @@
 
 #define LED_GPIO_NUM 33
 
-// ===========================
-// Enter your WiFi credentials
-// ===========================
+#define PART_BOUNDARY "123456789000000000000987654321"
+static const char *_STREAM_CONTENT_TYPE =
+    "multipart/x-mixed-replace;boundary=" PART_BOUNDARY;
+static const char *_STREAM_BOUNDARY = "\r\n--" PART_BOUNDARY "\r\n";
+static const char *_STREAM_PART = "Content-Type: image/jpeg\r\nContent-Length: "
+                                  "%u\r\nX-Timestamp: %d.%06d\r\n\r\n";
+
 const char *ssid = "yohan_phone";
 const char *pass = "testing123";
 int secondsElapsed = 0;
 
-void startCameraServer();
+httpd_handle_t stream_httpd = NULL;
+
+static esp_err_t stream_handler(httpd_req_t *req) {
+  camera_fb_t *fb = NULL;
+  esp_err_t res = ESP_OK;
+  struct timeval _timestamp;
+  int _jpg_buf_len = 0;
+  uint8_t *_jpg_buf = NULL;
+  char *part_buf[64];
+
+  res = httpd_resp_set_type(req, _STREAM_CONTENT_TYPE);
+  if (res != ESP_OK) {
+    return res;
+  }
+
+  while (true) {
+    fb = esp_camera_fb_get();
+    if (!fb) {
+      Serial.println("Camera capture failed");
+      res = ESP_FAIL;
+    } else {
+      /* Serial.println("Camera capture success"); */
+      _timestamp.tv_sec = fb->timestamp.tv_sec;
+      _timestamp.tv_usec = fb->timestamp.tv_usec;
+
+      /* if (fb->width > 400) { */
+      /*   if (fb->format != PIXFORMAT_JPEG) { */
+      /*     bool jpeg_converted = frame2jpg(fb, 80, &_jpg_buf, &_jpg_buf_len);
+       */
+      /*     esp_camera_fb_return(fb); */
+      /*     fb = NULL; */
+      /*     if (!jpeg_converted) { */
+      /*       Serial.println("JPEG compression failed"); */
+      /*       res = ESP_FAIL; */
+      /*     } */
+      /*   } else { */
+      /*     _jpg_buf_len = fb->len; */
+      /*     _jpg_buf = fb->buf; */
+      /*   } */
+      /* } */
+      _jpg_buf_len = fb->len;
+      _jpg_buf = fb->buf;
+    }
+
+    if (res == ESP_OK) {
+      res = httpd_resp_send_chunk(req, _STREAM_BOUNDARY,
+                                  strlen(_STREAM_BOUNDARY));
+    }
+    if (res == ESP_OK) {
+      size_t hlen = snprintf((char *)part_buf, 128, _STREAM_PART, _jpg_buf_len,
+                             _timestamp.tv_sec, _timestamp.tv_usec);
+      res = httpd_resp_send_chunk(req, (const char *)part_buf, hlen);
+    }
+    if (res == ESP_OK) {
+      res = httpd_resp_send_chunk(req, (const char *)_jpg_buf, _jpg_buf_len);
+    }
+
+    if (fb) {
+      esp_camera_fb_return(fb);
+      fb = NULL;
+      _jpg_buf = NULL;
+    } else if (_jpg_buf) {
+      free(_jpg_buf);
+      _jpg_buf = NULL;
+    }
+    if (res != ESP_OK) {
+      log_e("Send frame failed");
+      break;
+    }
+
+    /* Serial.printf("MJPG: %uB\n", (uint32_t)(_jpg_buf_len)); */
+  }
+  return res;
+}
+
+void startCameraServer() {
+  httpd_config_t config = HTTPD_DEFAULT_CONFIG();
+  config.server_port = 80;
+
+  httpd_uri_t index_uri = {.uri = "/",
+                           .method = HTTP_GET,
+                           .handler = stream_handler,
+                           .user_ctx = NULL};
+
+  // Serial.printf("Starting web server on port: '%d'\n", config.server_port);
+  if (httpd_start(&stream_httpd, &config) == ESP_OK) {
+    httpd_register_uri_handler(stream_httpd, &index_uri);
+  }
+}
 
 int connectToWiFi() {
 
@@ -88,7 +186,7 @@ int connectToWiFi() {
     delay(5000);
     Serial.print(".");
   }
-  Serial.println("CONNECTED");
+  Serial.println(" CONNECTED");
 
   return 0;
 }
@@ -120,27 +218,28 @@ void setup() {
   config.pin_pwdn = PWDN_GPIO_NUM;
   config.pin_reset = RESET_GPIO_NUM;
   config.xclk_freq_hz = 20000000;
-  config.frame_size = FRAMESIZE_UXGA;
+  config.frame_size = FRAMESIZE_240X240;
   config.pixel_format = PIXFORMAT_JPEG; // for streaming */
   // config.pixel_format = PIXFORMAT_RGB565; // for face detection
   config.grab_mode = CAMERA_GRAB_WHEN_EMPTY;
   config.fb_location = CAMERA_FB_IN_PSRAM;
-  config.jpeg_quality = 12;
+  config.jpeg_quality = 10;
   config.fb_count = 1;
 
   if (config.pixel_format == PIXFORMAT_JPEG) {
     if (psramFound()) {
+      Serial.println("PSRAM found");
       config.jpeg_quality = 10;
       config.fb_count = 2;
       config.grab_mode = CAMERA_GRAB_LATEST;
     } else {
       // Limit the frame size when PSRAM is not available
-      config.frame_size = FRAMESIZE_SVGA;
+      /* config.frame_size = FRAMESIZE_SVGA; */
       config.fb_location = CAMERA_FB_IN_DRAM;
     }
   } else {
     // Best option for face detection/recognition
-    config.frame_size = FRAMESIZE_240X240;
+    /* config.frame_size = FRAMESIZE_240X240; */
 #if CONFIG_IDF_TARGET_ESP32S3
     config.fb_count = 2;
 #endif
@@ -180,18 +279,23 @@ void setup() {
   connectToWiFi();
 
   startCameraServer();
+  /* int res = startCameraServer(); */
+  /* if (res == 0) { */
   Serial.print("Camera Ready! Use 'http://");
   Serial.print(WiFi.localIP());
   Serial.println("' to connect");
+  /* } else { */
+  /*   Serial.println("Camera Server failed to start"); */
+  /* } */
 }
 
 void loop() {
 
   // toggle led every 1 second
-  digitalWrite(LED_GPIO_NUM, HIGH);
-  delay(300);
-  digitalWrite(LED_GPIO_NUM, LOW);
-  delay(300);
+  /* digitalWrite(LED_GPIO_NUM, HIGH); */
+  /* delay(300); */
+  /* digitalWrite(LED_GPIO_NUM, LOW); */
+  delay(10000);
 
   /* printf("seconds elapsed: %d", ++secondsElapsed); */
   /* Serial.println("seconds elapsed: " + String(++secondsElapsed)); */
