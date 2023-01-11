f2c75ec6285c1390da2da1454d168cb06b794332
Yohan de Rose <yohanderose@gmail.com>
2023-01-11 17:08:08 +0700

    feat: Adding more info to the UI and and online_set to hold devices with an ip, this is checked every 5s and updated depending on what devices are reachable

--- api/v2_app.py
+++ api/v2_app.py
@@ -19,6 +19,12 @@ from multiprocessing import Pool, Process
 from concurrent.futures import ThreadPoolExecutor
 import PySimpleGUI as sg
 
+import matplotlib.pyplot as plt
+import dash
+from dash.dependencies import Output, Input
+from dash import Dash, html, dcc
+import plotly.express as px
+
 
 load_dotenv()
 
@@ -37,6 +43,7 @@ print(
     f"Stacked grid of image dimension {default_id_img.shape} \n-- {num_imgs} arranged {arangement} ")
 
 state_dict = dict(zip(sound_sensors, [False] * len(sound_sensors)))
+online_set = set()
 
 
 def find_audio_files() -> dict:
@@ -79,11 +86,13 @@ def ping_ip_for_id(ip) -> int:
 
 
 def _assign_exhibitarea_IP(ip):
-    global area_obj_map
+    global area_obj_map, online_set
+
     try:
         id = ping_ip_for_id(ip)
-        if id > 0 and id <= 16:
+        if id > 0 and id <= 16 and id not in online_set:
             print(f"Found device with id {id} at {ip}")
+            online_set.add(id)
             while True:
                 area_obj_map[id].ip = ip
                 area_obj_map[id].video_stream = cv2.VideoCapture(
@@ -97,20 +106,22 @@ def _assign_exhibitarea_IP(ip):
 
 
 def assign_exhibitarea_IPs():
-    host_ip = ".".join(HOST_IP.split(".")[:-1])
-    cmd = f"nmap -sn --system-dns {host_ip}.0/24 -oG -" + \
-        " | grep 'Status: Up' | awk '{print $2}' "
-    print(cmd)
-    p = Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE)
-    out, err = p.communicate()
-    out = out.decode("utf-8").split('\n')
-    if len(err) > 0:
-        print(err)
-        return 1
-    res = []
-    with ThreadPoolExecutor(max_workers=32) as executor:
-        res = executor.map(_assign_exhibitarea_IP, out)
-    return res
+    global online_set
+
+    while True:
+        host_ip = ".".join(HOST_IP.split(".")[:-1])
+        cmd = f"nmap -sn --system-dns {host_ip}.0/24 -oG -" + \
+            " | grep 'Status: Up' | awk '{print $2}' "
+        # print(cmd)
+        p = Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE)
+        out, err = p.communicate()
+        out = out.decode("utf-8").split('\n')
+        if len(err) > 0:
+            print(err)
+        with ThreadPoolExecutor(max_workers=32) as executor:
+            executor.map(_assign_exhibitarea_IP, out)
+
+        time.sleep(5)
 
 
 playlist = find_audio_files()
@@ -125,51 +136,64 @@ area_thread_map = {sound_id: Thread(
 for i in range(1, num_imgs+1):
     write_slice_from_id(default_id_img, i, data=i)
 area_state_img = default_id_img.copy()
-
-sg.theme("LightGreen")
-sg_ui_window = sg.Window('Exhibit Area Overview', [
-    [sg.Image(filename='', key='image')]], location=(800, 400))
+png_area_img = cv2.imencode(
+    '.png', area_state_img)[1].tobytes()
 
 
 def write_frames_from_devices(area: ExhibitAreaV2):
-    global sg_ui_window
+    global png_area_img, online_set
 
-    if area.ip is not None:  # Device  detected
-        # print(f'Area {area.sound_id}')
-        try:
-            if area.video_stream:
-                ret, frame = area.video_stream.read()
-                if ret:
-                    # Crop to square and convert to greyscale
-                    frame = frame[-IMG_HEIGHT:, -IMG_WIDTH:, 0]
-                    # # Add Blur
-                    # frame = cv2.GaussianBlur(frame, (5, 5), 0)
-                    write_slice_from_id(
-                        area_state_img, area.sound_id, frame)
-                    # sg_ui_window.read(timeout=0)
-                    # sg_ui_window['image'].update(data=cv2.imencode(
-                    #     '.png', area_state_img)[1].tobytes())
-                else:
-                    if area.video_stream:
-                        area.video_stream.release()
-                        area.video_stream = None
-                        area.video_stream = cv2.VideoCapture(
-                            'http://' + area.ip)
-        except Exception as e:
-            print(e)
+    while True:
+        if area.ip is not None:  # Device  detected
+            # print(f'Area {area.sound_id}')
+            try:
+                if area.video_stream:
+                    ret, frame = area.video_stream.read()
+                    if ret:
+                        # Crop to square and convert to greyscale
+                        frame = frame[-IMG_HEIGHT:, -IMG_WIDTH:, 0]
+                        # # Add Blur
+                        # frame = cv2.GaussianBlur(frame, (5, 5), 0)
+                        write_slice_from_id(
+                            area_state_img, area.sound_id, frame)
+
+                        png_area_img = cv2.imencode(
+                            '.png', area_state_img)[1].tobytes()
+                    else:
+                        if area.video_stream:
+                            area.video_stream.release()
+                            area.video_stream = None
+                            area.video_stream = cv2.VideoCapture(
+                                'http://' + area.ip)
+            except Exception as e:
+                online_set.remove(area.sound_id)
+                print(e)
+
+
+sg.theme("DarkBrown1")
+sg_ui_window = sg.Window('Exhibit Area Overview', [
+    [sg.Image(data='', key='image')],
+    [sg.Text("Online")],
+    [sg.Text(f"{online_set}", key='online')],
+    [sg.Text("Person detected")],
+    [sg.Text(f"{state_dict}", key='states')]
+], location=(800, 400))
 
 
 def read_frames_from_devices():
-    global area_state_img, area_obj_map, sg_ui_window
+    global area_state_img, area_obj_map, sg_ui_window, state_dict
+
+    reader_threads = [Thread(target=assign_exhibitarea_IPs)] + [Thread(
+        target=write_frames_from_devices, args=(area,)) for area in area_obj_map.values()]
+
+    for t in reader_threads:
+        t.start()
 
     while True:
-        # for area in area_obj_map.values():
-        #     write_frames_from_devices(area)
-        with ThreadPoolExecutor(max_workers=16) as exe:
-            exe.map(write_frames_from_devices, area_obj_map.values())
-        sg_ui_window.read(timeout=0)
-        sg_ui_window['image'].update(data=cv2.imencode(
-            '.png', area_state_img)[1].tobytes())
+        sg_ui_window.read(timeout=10)
+        sg_ui_window['image'].update(png_area_img)
+        sg_ui_window['states'].update(value=f"{state_dict}")
+        sg_ui_window['online'].update(value=f"{online_set}")
 
 
 def apply_state_to_system():
@@ -211,7 +235,8 @@ def detect_update_state():
                 state_dict = _state
                 # apply_state_to_system()
         except Exception as e:
-            print("OPENCV ERROR DETECTING CONTOURS -- ", e)
+            # print("OPENCV ERROR DETECTING CONTOURS -- ", e)
+            pass
 
 
 def handle_sigkill(signum, frame):
@@ -237,39 +262,50 @@ def generate_UI_frames():
                b'Content-Type: image/jpeg\r\n\r\n' + ui_frame + b'\r\n')
         ui_frame = cv2.imencode('.jpg', area_state_img)[1].tobytes()
 
-# # # -------------------------------- FLASK API --------------------------------
+# # # -------------------------------- API --------------------------------
 
 
-app = Flask(__name__)
-CORS(app)
+# app = Dash(__name__)
 
 
-@ app.route('/')
-def index():
-    return "Hello World"
+# @app.callback(
+#     Output('live-graph', 'children'),
+#     [Input('graph-update', 'n_intervals')]
+# )
+# def display_area_overview(n):
+#     global area_state_img
+
+#     fig = px.imshow(area_state_img, color_continuous_scale="gray")
+#     fig.update_layout(coloraxis_showscale=False)
+#     fig.update_xaxes(showticklabels=False)
+#     fig.update_yaxes(showticklabels=False)
+
+#     return dcc.Graph(figure=fig)
 
 
-@ app.route('/visual')
-def visualise():
-    return Response(generate_UI_frames(),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
+# app.layout = html.Div([
+#     html.H1("Exhibit Overview"),
+
+#     html.Div(
+#         [
+#             html.Div(id='live-graph'),
+#             dcc.Interval(
+#                 id='graph-update',
+#                 interval=100,
+#                 n_intervals=0
+#             ),
+#         ]
+#     )
+# ])
+
+# app.run(debug=True, host=f'{HOST_IP}', port=5000)
 
 
 # write_slice_from_id(area_state_img, 1, data=200)
 # print(ping_ip_for_id('172.20.10.2'))
 # print(ping_ip_for_id('172.20.10.4'))
 # read_frames_from_devices()
-assign_exhibitarea_IPs()
 cv_consumer = Process(target=read_frames_from_devices)
 cv_consumer.start()
-# cv_detector = Thread(target=detect_update_state)
-# cv_detector.start()
-# app.run(debug=True, host=f'{HOST_IP}', port=5000)
-
-
-# for i in range(200):
-#     sg_ui_window.read(timeout=20)
-#     sg_ui_window['image'].update(data=cv2.imencode(
-#         '.png', area_state_img)[1].tobytes())
-#     time.sleep(0.1)
-#     write_slice_from_id(area_state_img, 1, data=i+1)
+cv_detector = Process(target=detect_update_state)
+cv_detector.start()
